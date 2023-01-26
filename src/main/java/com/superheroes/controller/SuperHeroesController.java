f34eaa98536252be84a70ead65d0e8133a4ef977
ldoglioli86 <ldoglioli@gmail.com>
2023-01-26 18:20:52 -0300

    LDSH-4_add_SuperHeroesController

---
+++ src/main/java/com/superheroes/controller/SuperHeroesController.java
@@ -0,0 +1,19 @@
+package com.superheroes.controller;
+
+import com.superheroes.model.SuperHero;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/super-heroes")
+public class SuperHeroesController {
+
+    @GetMapping
+    public List<SuperHero> getSuperHeroes() {
+        return new ArrayList<>();
+    }
+}
