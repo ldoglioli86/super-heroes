cfad59bf59b2c8b400f32b60a94e9660e70cf093
ldoglioli86 <ldoglioli@gmail.com>
2023-01-27 19:16:39 -0300

    LDSH-7_add_persistence_annotations_to_model_classes

--- src/main/java/com/superheroes/model/Publisher.java
+++ src/main/java/com/superheroes/model/Publisher.java
@@ -1,7 +1,19 @@
 package com.superheroes.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "publisher")
 public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(nullable = false)
     Long name;
 
     public Long getId() {
--- src/main/java/com/superheroes/model/SuperHero.java
+++ src/main/java/com/superheroes/model/SuperHero.java
@@ -4,12 +4,31 @@ import com.superheroes.model.enums.Alignment;
 import com.superheroes.model.enums.Gender;
 import com.superheroes.model.enums.Race;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "super_hero")
 public class SuperHero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private Gender gender;
+    @Column(nullable = false)
     private Race race;
+    @Column(nullable = false)
     private Alignment alignment;
+    @ManyToOne
+    @JoinColumn(name = "publisherId")
     private Publisher publisher;
 
     public Long getId() {
