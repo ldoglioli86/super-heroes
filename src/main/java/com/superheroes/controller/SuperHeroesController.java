ccb4960f55c83f42a8f24c983763d19c86eabf0d
ldoglioli86 <ldoglioli@gmail.com>
2023-01-31 12:51:50 -0300

    LDSH-12: Add LogExecutionTime annotation to Controller method

--- src/main/java/com/superheroes/controller/SuperHeroesController.java
+++ src/main/java/com/superheroes/controller/SuperHeroesController.java
@@ -1,5 +1,6 @@
 package com.superheroes.controller;
 
+import com.superheroes.aspect.LogExecutionTime;
 import com.superheroes.model.SuperHero;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 public class SuperHeroesController {
 
     @GetMapping
+    @LogExecutionTime
     public List<SuperHero> getSuperHeroes() {
         return new ArrayList<>();
     }
