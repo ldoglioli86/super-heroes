7d8b6a9e43906a614cd71ed4d13872d6cfa14d25
ldoglioli86 <ldoglioli@gmail.com>
2023-02-02 13:56:45 -0300

    LDSH-26: add DB repositories for Super Heroes

--- src/main/java/com/superheroes/repository/PublisherRepository.java
+++ src/main/java/com/superheroes/repository/PublisherRepository.java
@@ -2,6 +2,8 @@ package com.superheroes.repository;
 
 import com.superheroes.model.Publisher;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
--- src/main/java/com/superheroes/repository/SuperHeroRepository.java
+++ src/main/java/com/superheroes/repository/SuperHeroRepository.java
@@ -2,6 +2,8 @@ package com.superheroes.repository;
 
 import com.superheroes.model.SuperHero;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SuperHeroRepository extends CrudRepository<SuperHero, Long> {
 }
